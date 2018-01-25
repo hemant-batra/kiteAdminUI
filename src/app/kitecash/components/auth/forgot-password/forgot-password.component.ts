75a76fa45191c2f5ae35827d7140270ff0783375
hemant-batra <hemant.batra@theinnovationinc.co>
2018-01-26 04:12:01 +0530

    Changes for Kite Tab

---
+++ src/app/kitecash/components/auth/forgot-password/forgot-password.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {SessionService} from '../../../services/common/session.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent implements OnInit {
+
+  constructor(private session: SessionService,
+              private router: Router) {}
+
+  ngOnInit() {
+    if (this.session.getUserName() === null) {
+      this.router.navigate(['']);
+    }
+  }
+}
